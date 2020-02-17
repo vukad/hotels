6de7f85ba4c1b55d9ecf825ec0ce684aadeb36b1
vukad <sthacyman@gmail.com>
2020-02-17 01:01:39 +0000

    20200117

---
+++ Data/entry.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+	<meta charset="UTF-8">
+	<title>ホテル検索</title>
+	<link rel="stylesheet" href="../assets/css/style.css" />
+	<link rel="stylesheet" href="../assets/css/hotels.css" />
+</head>
+
+<body>
+	<header>
+		<h1>ホテルの検索</h1>
+	</header>
+	<main>
+		<article>
+			<p>ホテルの所在地を入力してください。所在地の一部でも構いません。</p>
+			<form action="list.php" method="get">
+				<input type="text" name="address" />
+				<input type="submit" value="検索" />
+			</form>
+		</article>
+	</main>
+	<footer>
+		<div id="copyright">(C) 2019 The Web System Development Course</div>
+	</footer>
+</body>
+
+</html>
---
+++ Data/list.php
@@ -0,0 +1,177 @@
+<?php
+//step1 
+$area = -1;
+if (isset($_REQUEST["area"])) {
+    $area = $_REQUEST["area"];
+}
+//step2 lay du lieu database
+$pdo = connectDatabase();
+//step3 chay du lieu Sql
+$sql = "select * from restaurants where area=:area?"; //$sql - "select * from restaurants where area=:area?";
+//step4 chuyen du an vao sql
+$pstmt = $pdo->prepare($sql);
+//step5 dieu chinh thong tin trong sql
+$psttmt->bindValue(1,$area);
+$params[":area"] = $area;
+//step6  chay SQL
+$psttmt->execute($params);
+//step7 set ket qua
+$rs = $psttmt->fetchAll();
+//step8 cho ket qua vao day 
+disconnectDatabase($pdo);
+$restaurant = [];
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+	<meta charset="UTF-8">
+	<title>ホテル検索結果一覧</title>
+	<link rel="stylesheet" href="../assets/css/style.css" />
+	<link rel="stylesheet" href="../assets/css/hotels.css" />
+</head>
+
+<body>
+	<header>
+		<h1>ホテル検索結果一覧</h1>
+		<p><a href="./entry.php">検索ページに戻る</a></p>
+	</header>
+	<main>
+		<article>
+			<table>
+				<tr>
+					<td>
+						<img src="../images/1.png" width="100" />
+					</td>
+					<td>
+						<table class="detail">
+							<tr>
+								<td>ビジネスホテル大井町<br /></td>
+							</tr>
+							<tr>
+								<td>東京都品川区大井 11-11-11</td>
+							</tr>
+							<tr>
+								<td>宿泊料：&yen;7,000</td>
+							</tr>
+							<tr>
+								<td></td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<img src="../images/2.png" width="100" />
+					</td>
+					<td>
+						<table class="detail">
+							<tr>
+								<td>グレースイン蒲田<br /></td>
+							</tr>
+							<tr>
+								<td>東京都大田区蒲田 11-11-11</td>
+							</tr>
+							<tr>
+								<td>宿泊料：&yen;7,200</td>
+							</tr>
+							<tr>
+								<td></td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<img src="../images/3.png" width="100" />
+					</td>
+					<td>
+						<table class="detail">
+							<tr>
+								<td>ビジネスイン赤坂見附<br /></td>
+							</tr>
+							<tr>
+								<td>東京都港区赤坂 11-11-11</td>
+							</tr>
+							<tr>
+								<td>宿泊料：&yen;9,500</td>
+							</tr>
+							<tr>
+								<td></td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<img src="../images/4.png" width="100" />
+					</td>
+					<td>
+						<table class="detail">
+							<tr>
+								<td>西新宿ステーションホテル<br /></td>
+							</tr>
+							<tr>
+								<td>東京都新宿区西新宿 11-11-11</td>
+							</tr>
+							<tr>
+								<td>宿泊料：&yen;8,500</td>
+							</tr>
+							<tr>
+								<td>最寄駅：新宿駅、西新宿駅から徒歩5分</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<img src="../images/5.png" width="100" />
+					</td>
+					<td>
+						<table class="detail">
+							<tr>
+								<td>ホテル蒲田IN<br /></td>
+							</tr>
+							<tr>
+								<td>東京都大田区蒲田 22-22-22</td>
+							</tr>
+							<tr>
+								<td>宿泊料：&yen;6,200</td>
+							</tr>
+							<tr>
+								<td></td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<img src="../images/6.png" width="100" />
+					</td>
+					<td>
+						<table class="detail">
+							<tr>
+								<td>ホテル南新宿<br /></td>
+							</tr>
+							<tr>
+								<td>東京都新宿区南新宿 11-11-11</td>
+							</tr>
+							<tr>
+								<td>宿泊料：&yen;5,500</td>
+							</tr>
+							<tr>
+								<td>最寄駅：新宿駅東口から徒歩9分</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+			</table>
+		</article>
+	</main>
+	<footer>
+		<div id="copyright">(C) 2019 The Web System Development Course</div>
+	</footer>
+</body>
+
+</html>
